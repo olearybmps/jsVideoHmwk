c22a44a57a9cb5d9a8fb2b82bc653db8bd76bda1
olearybm <90865347+olearybm@users.noreply.github.com>
2024-03-15 19:14:59 -0400

    30 minutes in

---
+++ app.js
@@ -0,0 +1,58 @@
+// console.log
+// console.log('Hello World');
+// console.error('This is an error');
+// console.warn('This is an error');
+//***************************************
+// variables
+//***************************************
+// let score = 30;
+// const age = 30;
+//***************************************
+// data types string numbers boolean null undefined 
+// const name = 'John';
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const  = null;
+// const y = undefined;
+// let z;
+//***************************************
+// concatenation
+//***************************************
+//console.log('My name is ' + name + 'and I am ' + age);
+// console.log(`My name is ${name} and I am ${age}`);
+//const s = 'Hello World!';
+// const s = 'technology, computers, it, code';
+// console.log(s.length);
+// console.log(s.toUpperCase());
+// console.log(s.substring(0,5).toLowerCase());
+// console.log(s.split(', '));
+//***************************************
+// Arrays - variables that hold multiple values
+//***************************************
+// const numbers = new Array('1,2,3,4,5');
+// console.log(numbers);
+//const fruits = ['apples', 'oranges', 'pears', 10, true];
+// const fruits = ['apples', 'oranges', 'pears'];
+// fruits[3] = 'grapes';
+// fruits.push('mangos');
+// fruits.unshift('strawberries');
+// fruits.pop();
+// console.log(Array.isArray(fruits));
+// fruits.indexOf('oranges');
+// console.log(fruits);
+//***************************************
+// Object literals
+//***************************************
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+console.log(person);
